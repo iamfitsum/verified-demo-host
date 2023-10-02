801f478195741cdd309b3655079f13b1f64da3ac
Fitsum Mekonnen <official.fitsum.mekonnen@gmail.com>
2023-10-02 04:55:39 +0300

    Finishing Touches + Code Cleanup

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     federation({
       name: "app",
       remotes: {
-        remoteApp: "http://localhost:5001/assets/remoteEntry.js",
+        remoteApp: "https://verified-demo-remote.vercel.app/assets/remoteEntry.js",
       },
       shared: ["react", "react-dom"],
     }),
